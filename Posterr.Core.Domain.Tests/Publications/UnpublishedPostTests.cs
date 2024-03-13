a667cefca263b0342dba5865e9f3e55b54dec6af
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-03-13 07:48:41 -0300

    UnpublishedPost entity created

---
+++ Posterr.Core.Domain.Tests/Publications/UnpublishedPostTests.cs
@@ -0,0 +1,35 @@
+﻿using Posterr.Core.Domain.Publications;
+using Posterr.Core.Domain.Publications.Exceptions;
+using Posterr.Core.Domain.Users;
+
+namespace Posterr.Core.Domain.Tests.Publications;
+
+public class UnpublishedPostTests
+{
+    [Fact]
+    public void GivenValidParameters_WhenInstantiatingUnpublishedPostEntity_ThenSucceed()
+    {
+        var user = new User("username");
+        var content = "content";
+        var unpublishedPost = new UnpublishedPost(user, content);
+        Assert.Equal(user, unpublishedPost.Author);
+        Assert.Equal(content, unpublishedPost.Content);
+    }
+
+#pragma warning disable CS8604 // Possible null reference argument.
+    [Fact]
+    public void GivenNullAuthor_WhenInstantiatingUnpublishedPostEntity_ThenThrowException()
+    {
+        Assert.Throws<ArgumentNullException>(() => new UnpublishedPost(null, "content"));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void GivenEmptyPostContent_WhenInstantiatingUnpublishedPostEntity_ThenThrowException(string? content)
+    {
+        Assert.Throws<EmptyPostContentException>(() => new UnpublishedPost(new User("username"), content));
+    }
+#pragma warning restore CS8604 // Possible null reference argument.
+}
---
+++ Posterr.Core.Domain/Publications/UnpublishedPost.cs
@@ -0,0 +1,17 @@
+﻿using Posterr.Core.Domain.Users;
+
+namespace Posterr.Core.Domain.Publications;
+
+public sealed record UnpublishedPost
+{
+    private readonly PostContent _content;
+    public string Content { get => _content.Value; }
+    public User Author { get; }
+
+    public UnpublishedPost(User author, string content)
+    {
+        ArgumentNullException.ThrowIfNull(author);
+        _content = new PostContent(content);
+        Author = author;
+    }
+}
