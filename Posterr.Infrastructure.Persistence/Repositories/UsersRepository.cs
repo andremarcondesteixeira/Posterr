4b87e05cc993712d7048a12765b3b0925b50d2a8
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-03-18 08:32:28 -0300

    FindByUsername implemented

--- Posterr.Infrastructure.Persistence/Repositories/UsersRepository.cs
+++ Posterr.Infrastructure.Persistence/Repositories/UsersRepository.cs
@@ -1,12 +1,20 @@
-﻿using Posterr.Core.Application.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Posterr.Core.Application.Interfaces;
 using Posterr.Core.Domain.Boundaries.Persistence;
 
 namespace Posterr.Infrastructure.Persistence.Repositories;
 
-public class UsersRepository : IUsersRepository
+public class UsersRepository(ApplicationDbContext dbContext) : IUsersRepository
 {
     public Task<IUser?> FindByUsername(string username)
     {
-        throw new NotImplementedException();
+        var user = dbContext.Users.Where(u => u.Username == username).First();
+
+        if (user is null)
+        {
+            return Task.FromResult<IUser?>(null);
+        }
+
+        return Task.FromResult<IUser?>(user.ToIUser());
     }
 }
