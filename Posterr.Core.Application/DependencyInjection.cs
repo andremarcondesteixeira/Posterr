8d3d62ea704961fa5c5b210a8d1869b928334df8
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-03-18 00:40:00 -0300

    Creates extension method to automatically registers use cases in dependency injection container

---
+++ Posterr.Core.Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace Posterr.Core.Application;
+
+public static class DependencyInjection
+{
+    // This will automatically register all use cases in the DI Container
+    public static void ConfigureUseCases(this IServiceCollection services)
+    {
+        var useCases = typeof(DependencyInjection).Assembly
+            .GetTypes()
+            .Where(type =>
+                !type.GetTypeInfo().IsInterface
+                && !type.GetTypeInfo().IsAbstract
+                && type != typeof(DependencyInjection)
+                && type.Namespace != nameof(Exceptions)
+             );
+
+        foreach (var useCase in useCases)
+        {
+            services.AddTransient(useCase);
+        }
+    }
+}
