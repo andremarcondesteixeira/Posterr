494a03a3ae787c7b9017d416f5cbeaf6f7cebd94
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-03-20 15:10:41 -0300

    ListPosts endpoint created

---
+++ Posterr.Presentation.API.Controllers/PublicationsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Posterr.Core.Application.UseCases.CreateNewPost;
+using Posterr.Core.Application.UseCases.PaginatePublications;
+
+namespace Posterr.Presentation.API.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+public class PublicationsController(
+    CreateNewPostUseCase createNewPostUseCase,
+    PaginatePublicationsUseCase paginatePublicationsUseCase
+) : ControllerBase
+{
+    [HttpPost]
+    public Task<CreateNewPostResponse> CreateNewPost([FromBody] CreateNewPostRequest request) =>
+        createNewPostUseCase.Run(request);
+
+    [HttpGet]
+    public Task<IList<PaginatePublicationsResponseItem>> ListPosts([FromBody] PaginatePublicationsRequest request) =>
+        paginatePublicationsUseCase.Run(request);
+}
