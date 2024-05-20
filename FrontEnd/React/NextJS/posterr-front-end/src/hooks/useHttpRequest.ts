d05bbf1c4a25c30cd14c2b99d50e8dd693d7ec5e
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 08:55:45 -0300

    removes Result usage from useHttpRequests, since isLoading will force data to be undefined anyway

--- FrontEnd/React/NextJS/posterr-front-end/src/hooks/useHttpRequest.ts
+++ FrontEnd/React/NextJS/posterr-front-end/src/hooks/useHttpRequest.ts
@@ -1,26 +1,16 @@
 import { Result } from "@Core/Util/Result";
 import useSWR from "swr";
 
-// I decided to use a Result wrapper here only to remove the undefineds from the type definition
-export type ApiResponse<RESPONSE_TYPE> = Result<{
-  data: RESPONSE_TYPE;
-  isLoading: boolean;
-}, Error>;
-
 /*
 This function is an isolation layer between the application's code and the outside world.
 It allows us to chose whatever fetching library we want, be it useSWR, Tanstack Query, vanilla fetch calls, etc...
 Whatever fetching approach is used, it will not impact the application's architecture, because it is safely contained here
 */
-export function useHttpRequest<RESPONSE_TYPE>(requestId: string, fetcher: () => Promise<Result<RESPONSE_TYPE, Error>>): ApiResponse<RESPONSE_TYPE> {
+export function useHttpRequest<RESPONSE_TYPE>(requestId: string, fetcher: () => Promise<Result<RESPONSE_TYPE, Error>>) {
   const { data, error, isLoading } = useSWR<RESPONSE_TYPE, Error, string>(requestId, async () => {
     const response = await fetcher();
     return response.okOrThrow();
   });
 
-  if (error) {
-    return Result.Error(error);
-  }
-
-  return Result.Ok({ data, isLoading }) as ApiResponse<RESPONSE_TYPE>;
+  return { data, error, isLoading };
 }
