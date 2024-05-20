7aa263cef4e82676901b6f44bedf9773e4099898
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 13:41:08 -0300

    page considers loading state and error when loading posts

--- FrontEnd/React/NextJS/posterr-front-end/src/app/page.tsx
+++ FrontEnd/React/NextJS/posterr-front-end/src/app/page.tsx
@@ -9,11 +9,17 @@ export default function Home() {
     <main>
       <h1>Home</h1>
       <ul>
+        {isLoadingPosts && (
+          <span>Loading posts...</span>
+        )}
         {posts && posts.map(post => (
           <li key={`${post.postId}-${post.repostId ?? 'original'}`}>
             {post.publicationDate.toLocaleDateString()} - {post.authorUsername} - {post.content}
           </li>
         ))}
+        {errorLoadingPosts && (
+          <span>There was an error when loading the posts: {errorLoadingPosts.message}</span>
+        )}
       </ul>
     </main>
   );
