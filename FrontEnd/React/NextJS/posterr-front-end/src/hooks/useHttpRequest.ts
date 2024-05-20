9926f1662969903fb47b64f144c0024be60cae0e
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 08:43:05 -0300

    typo

--- FrontEnd/React/NextJS/posterr-front-end/src/hooks/useHttpRequest.ts
+++ FrontEnd/React/NextJS/posterr-front-end/src/hooks/useHttpRequest.ts
@@ -8,7 +8,7 @@ export type ApiResponse<RESPONSE_TYPE> = Result<{
 }, Error>;
 
 /*
-This function is a isolation layer between the application's code and the outside world.
+This function is an isolation layer between the application's code and the outside world.
 It allows us to chose whatever fetching library we want, be it useSWR, Tanstack Query, vanilla fetch calls, etc...
 Whatever fetching approach is used, it will not impact the application's architecture, because it is safely contained here
 */
