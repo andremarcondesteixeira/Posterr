8e79fd93b85e93c5e1c3d71cb1664793282d19e5
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 15:24:17 -0300

    home page loading state test

--- FrontEnd/React/NextJS/posterr-front-end/src/app/__tests__/page.test.tsx
+++ FrontEnd/React/NextJS/posterr-front-end/src/app/__tests__/page.test.tsx
@@ -1,8 +1,37 @@
-import { expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import * as usePostsPaginationHook from "@/hooks/usePostsPagination";
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterAll, afterEach, describe, expect, test, vi } from 'vitest';
 import Page from '../page';
 
-test('Page', () => {
-  render(<Page />);
-  expect(screen.getByRole('heading', { level: 1, name: 'Posterr' })).toBeDefined();
+describe("Home", () => {
+  const usePostsPaginationSpy = vi.spyOn(usePostsPaginationHook, "usePostsPagination");
+
+  afterEach(() => {
+    usePostsPaginationSpy.mockReset();
+    cleanup();
+  });
+
+  afterAll(() => {
+    usePostsPaginationSpy.mockRestore();
+  });
+
+  test('It should render', () => {
+    usePostsPaginationSpy.mockReturnValueOnce({
+      errorLoadingPosts: undefined,
+      isLoadingPosts: true,
+      posts: undefined,
+    });
+    render(<Page />);
+    expect(screen.getByRole('heading', { level: 1, name: 'Posterr' })).toBeDefined();
+  });
+
+  test("It should display a loading state", () => {
+    usePostsPaginationSpy.mockReturnValueOnce({
+      errorLoadingPosts: undefined,
+      isLoadingPosts: true,
+      posts: undefined,
+    });
+    render(<Page />);
+    expect(screen.getByText('Loading posts...')).toBeInTheDocument();
+  });
 });
