abbfefc1aee97e68ad1beaa8324e644ef486b18c
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 03:48:06 -0300

    comment

--- FrontEnd/React/NextJS/posterr-front-end/src/hooks/useApiEndpoint.ts
+++ FrontEnd/React/NextJS/posterr-front-end/src/hooks/useApiEndpoint.ts
@@ -6,6 +6,11 @@ export type ApiResponse<RESPONSE_TYPE> = {
   error: unknown;
 }
 
+/*
+This function is a isolation layer between the application's code and the outside world.
+It allows us to chose whatever fetching library we want, be it useSWR, Tanstack Query, vanilla fetch calls, etc...
+Whatever fetching approach is used, it will not impact the application's architecture, because it is safely contained here
+*/
 export function useApiEndpoint<RESPONSE_TYPE>(url: string): ApiResponse<RESPONSE_TYPE> {
   const { data, error, isLoading } = useSWR<RESPONSE_TYPE>(url, async (url: string) => {
     const response = await fetch(url);
