d0e71bf5d5d82511c18eaef247c95afa433568f2
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 14:44:51 -0300

    .env added

--- FrontEnd/React/NextJS/posterr-front-end/next.config.mjs
+++ FrontEnd/React/NextJS/posterr-front-end/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/api/:slug*",
+        destination: `${process.env.API_SERVER_URL}/:slug*`,
+        permanent: true,
+      },
+    ];
+  }
+};
 
 export default nextConfig;
