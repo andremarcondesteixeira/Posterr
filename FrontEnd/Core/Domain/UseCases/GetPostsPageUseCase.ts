75e7801dcdd37a28d0f4f27a577c5ff113d1473a
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 07:26:34 -0300

    GetPostsPageUseCase created

--- FrontEnd/Core/Domain/UseCases/GetPostsPageUseCase.ts
+++ FrontEnd/Core/Domain/UseCases/GetPostsPageUseCase.ts
@@ -1,4 +1,11 @@
-export function getPostsPage(page: number) {
-  const pageSize = page === 1 ? 15 : 20;
+import { Publication } from "@Core/Domain/Entities/Publication";
+import { ApiEndpoint } from "@Core/Services/ApiEndpointsService";
+import { makeRequest } from "@Core/Services/HttpRequestService";
+import { Result } from "@Core/Util/Result";
 
+export type GetPostsPageUseCaseResponse = Promise<Result<Publication[], Error>>;
+
+export async function getPostsPage(page: number): GetPostsPageUseCaseResponse {
+  const pageSize = page === 1 ? 15 : 20;
+  return makeRequest<Publication[]>(`${ApiEndpoint.posts}?page=${page}&pageSize=${pageSize}`);
 }
