db082565a3563805f2916ed3f361736883559dea
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-20 03:45:12 -0300

    The application should have control of its own types

--- FrontEnd/React/NextJS/posterr-front-end/src/hooks/useApiEndpoint.ts
+++ FrontEnd/React/NextJS/posterr-front-end/src/hooks/useApiEndpoint.ts
@@ -1,6 +1,12 @@
 import useSWR from "swr";
 
-export function useApiEndpoint<RESPONSE_TYPE>(url: string) {
+export type ApiResponse<RESPONSE_TYPE> = {
+  data: RESPONSE_TYPE | undefined;
+  isLoading: boolean;
+  error: unknown;
+}
+
+export function useApiEndpoint<RESPONSE_TYPE>(url: string): ApiResponse<RESPONSE_TYPE> {
   const { data, error, isLoading } = useSWR<RESPONSE_TYPE>(url, async (url: string) => {
     const response = await fetch(url);
     return await response.json();
--- FrontEnd/React/NextJS/posterr-front-end/src/hooks/usePostsRequest.ts
+++ FrontEnd/React/NextJS/posterr-front-end/src/hooks/usePostsRequest.ts
@@ -2,6 +2,17 @@ import { Publication } from "@/core/domain/entities/Publication";
 import { ApiEndpoint } from "@/core/services/apiEndpointsService";
 import { useApiEndpoint } from "./useApiEndpoint";
 
-export function usePostsRequest(page: number) {
-  return useApiEndpoint<Publication>(`${ApiEndpoint.posts}?page=${page}`);
+export type PostRequestResponse = {
+  posts: Publication[],
+  isLoading: boolean;
+  error: unknown;
+}
+
+export function usePostsRequest(page: number): PostRequestResponse {
+  const { isLoading, data, error} = useApiEndpoint<Publication[]>(`${ApiEndpoint.posts}?page=${page}`);
+  return {
+    isLoading,
+    posts: data ?? [],
+    error
+  }
 }
