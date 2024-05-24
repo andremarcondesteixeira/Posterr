6e5666ca95a8ceff2ddba4e55c9e52ddb71d0c49
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-24 19:52:19 -0300

    introduces pagination configuration in DomainConfig

--- BackEnd/Core/Boundaries/Configuration/IDomainConfig.cs
+++ BackEnd/Core/Boundaries/Configuration/IDomainConfig.cs
@@ -4,4 +4,11 @@ public interface IDomainConfig
 {
     uint MaxPostLength { get; }
     ushort MaxAllowedDailyPublicationsByUser { get; }
+    IPaginationConfig Pagination { get; }
+
+    public interface IPaginationConfig
+    {
+        short FirstPageSize { get; }
+        short NextPagesSize { get; }
+    }
 }
--- BackEnd/Presentation/Web/RestApi/EntryPoint/DomainConfig.cs
+++ BackEnd/Presentation/Web/RestApi/EntryPoint/DomainConfig.cs
@@ -4,9 +4,26 @@ namespace Posterr.Presentation.Web.RestApi.EntryPoint;
 
 public sealed record DomainConfig(IConfiguration Configuration) : IDomainConfig
 {
-    public uint MaxPostLength =>
-        Configuration.GetRequiredSection("Domain").GetRequiredSection("MaxPostLength").Get<uint>();
+    public uint MaxPostLength => Configuration.GetRequiredSection("Domain")
+                                              .GetRequiredSection("MaxPostLength")
+                                              .Get<uint>();
 
-    public ushort MaxAllowedDailyPublicationsByUser =>
-        Configuration.GetRequiredSection("Domain").GetRequiredSection("MaxAllowedDailyPublicationsByUser").Get<ushort>();
+    public ushort MaxAllowedDailyPublicationsByUser => Configuration.GetRequiredSection("Domain")
+                                                                    .GetRequiredSection("MaxAllowedDailyPublicationsByUser")
+                                                                    .Get<ushort>();
+
+    public IDomainConfig.IPaginationConfig Pagination => new PaginationConfig(Configuration);
+
+    public sealed record PaginationConfig(IConfiguration Configuration) : IDomainConfig.IPaginationConfig
+    {
+        public short FirstPageSize => Configuration.GetRequiredSection("Domain")
+                                                   .GetRequiredSection("Pagination")
+                                                   .GetRequiredSection("FirstPageSize")
+                                                   .Get<short>();
+
+        public short NextPagesSize => Configuration.GetRequiredSection("Domain")
+                                                   .GetRequiredSection("Pagination")
+                                                   .GetRequiredSection("NextPagesSize")
+                                                   .Get<short>();
+    }
 }
