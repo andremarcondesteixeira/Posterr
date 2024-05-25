05bea0a9396b84e9a71bc7cb30fad9531691e204
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-25 14:38:23 -0300

    removes Error property from APIResource

--- BackEnd/Presentation/Web/RestApi/Controllers/HATEOAS/HAL/APIResource.cs
+++ BackEnd/Presentation/Web/RestApi/Controllers/HATEOAS/HAL/APIResource.cs
@@ -1,5 +1,3 @@
-using Posterr.Presentation.Web.RestApi.Controllers.Models;
-
 namespace Posterr.Presentation.Web.RestApi.Controllers.HATEOAS;
 
 // HAL Specification: https://datatracker.ietf.org/doc/html/draft-kelly-json-hal-11
@@ -7,11 +5,6 @@ namespace Posterr.Presentation.Web.RestApi.Controllers.HATEOAS;
 public abstract class APIResource
 {
     public Dictionary<string, List<APIResourceLinkDTO>> Links { get; } = [];
-    
-    // There is no error property in the HAL specification.
-    // I've put it here to facilitate returning error states in the Controllers.
-    // If all resources can have errors, then I don't need to change the type of the return statement when errors happen.
-    public IList<Error> Errors { get; } = [];
 }
 
 public abstract class APIResource<EMBEDDED> : APIResource where EMBEDDED : APIResource {
