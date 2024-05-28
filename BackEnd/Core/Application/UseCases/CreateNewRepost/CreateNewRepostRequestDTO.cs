c65a5ef0c59c9c5022e410eeadb63a125483c28c
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-28 03:04:28 -0300

    removes unnecessary validation

--- BackEnd/Core/Application/UseCases/CreateNewRepost/CreateNewRepostRequestDTO.cs
+++ BackEnd/Core/Application/UseCases/CreateNewRepost/CreateNewRepostRequestDTO.cs
@@ -1,14 +1,3 @@
 ﻿namespace Posterr.Core.Application.UseCases.CreateNewRepost;
 
-public sealed record CreateNewRepostRequestDTO
-{
-    public string AuthorUsername { get; }
-    public long OriginalPostId { get; }
-
-    public CreateNewRepostRequestDTO(string authorUsername, long originalPostId)
-    {
-        ArgumentException.ThrowIfNullOrWhiteSpace(authorUsername, nameof(authorUsername));
-        AuthorUsername = authorUsername;
-        OriginalPostId = originalPostId;
-    }
-}
+public sealed record CreateNewRepostRequestDTO(string AuthorUsername, long OriginalPostId);
