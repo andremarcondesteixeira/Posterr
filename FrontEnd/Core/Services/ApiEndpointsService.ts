1d135b56c61f667453cf1e6eadcabda6bd81be44
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-05-30 20:24:34 -0300

    APIEndpoint make its own HTTP requests now

--- FrontEnd/Core/Services/ApiEndpointsService.ts
+++ FrontEnd/Core/Services/ApiEndpointsService.ts
@@ -1,7 +1,30 @@
+import { Publication } from "../Domain/Entities/Publication";
+import { makeRequest } from "./HttpRequestService";
+
 const baseUrl = '/api';
 
-export enum ApiEndpoint {
-  posts = `${baseUrl}/posts`,
-  reposts = `${baseUrl}/reposts`,
-  publications = `${baseUrl}/publications`
-};
+export const ApiEndpoint = Object.freeze({
+  posts: Object.freeze({
+    url: `${baseUrl}/posts`,
+  }),
+  reposts: Object.freeze({
+    url: `${baseUrl}/reposts`,
+  }),
+  publications: Object.freeze({
+    url: `${baseUrl}/publications`,
+    GET: () => makeRequest<{
+      count: number;
+      _embedded: {
+        publications: Publication[];
+      };
+      _links: {
+        self: {
+          href: string;
+        };
+        next?: {
+          href: string;
+        }
+      }
+    }>(ApiEndpoint.publications.url),
+  }),
+});
