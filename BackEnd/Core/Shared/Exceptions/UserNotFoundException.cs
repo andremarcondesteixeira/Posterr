87550e8f8b9e0d158948d3af25dbc6a153e952dc
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-04 18:02:54 -0300

    UserNotFoundException got additional constructor

--- BackEnd/Core/Shared/Exceptions/UserNotFoundException.cs
+++ BackEnd/Core/Shared/Exceptions/UserNotFoundException.cs
@@ -1,9 +1,16 @@
 ﻿namespace Posterr.Core.Shared.Exceptions;
 
-public sealed class UserNotFoundException(string username)
-    : PosterrException(
+public sealed class UserNotFoundException : PosterrException
+{
+    public UserNotFoundException(string username) : base(
         $"No user found with username \"{username}\".",
         "Check if the user is actually persisted in the database and if you typed the username correctly."
     )
-{
+    { }
+
+    public UserNotFoundException(long userId) : base(
+        $"No user found with id {userId}.",
+        "Check if the user is actually persisted in the database and if you typed the user id correctly."
+    )
+    { }
 }
