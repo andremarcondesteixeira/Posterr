2244ae63da72bed131b28594d5a22981332e5994
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-04 14:34:00 -0300

    extracts PublicationAPIResourceDTO

---
+++ BackEnd/Presentation/Web/RestApi/Controllers/Models/PublicationAPIResourceDTO.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Posterr.Core.Boundaries.EntitiesInterfaces;
+using Posterr.Presentation.Web.RestApi.Controllers.HATEOAS.HAL;
+using Posterr.Presentation.Web.RestApi.Controllers.Users;
+using System.Text.Json.Serialization;
+
+namespace Posterr.Presentation.Web.RestApi.Controllers.Models;
+
+public sealed record PublicationAPIResourceDTO : APIResource<PublicationAPIResourceDTO.EmbeddedObjects>
+{
+    public required bool IsRepost { get; init; }
+    public long Id { get; }
+    public required string AuthorUsername { get; init; }
+    public required DateTime PublicationDate { get; init; }
+    public required string Content { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? OriginalPostAuthorUsername { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DateTime? OriginalPostPublicationDate { get; init; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? OriginalPostContent { get; init; }
+
+    public PublicationAPIResourceDTO(long id, string publicationsListEndpointUrl)
+    {
+        Id = id;
+        Links.Add("self", [new($"{publicationsListEndpointUrl}/{Id}")]);
+    }
+
+    public static PublicationAPIResourceDTO FromIPublication(IPublication publication, IUrlHelper urlHelper)
+    {
+        string listUsersUrl = urlHelper.ActionLink(nameof(UsersController.ListUsers), nameof(UsersController).Replace("Controller", ""))!;
+        string listPublicationsUrl = urlHelper.ActionLink(nameof(UsersController.ListUsers), nameof(UsersController).Replace("Controller", ""))!;
+
+        if (publication is IRepost repost)
+        {
+            return new PublicationAPIResourceDTO(publication.Id, listPublicationsUrl)
+            {
+                IsRepost = true,
+                AuthorUsername = publication.Author.Username,
+                PublicationDate = publication.PublicationDate,
+                Content = publication.Content,
+                OriginalPostAuthorUsername = repost.OriginalPost.Author.Username,
+                OriginalPostPublicationDate = repost.OriginalPost.PublicationDate,
+                OriginalPostContent = repost.OriginalPost.Content,
+                Embedded = new EmbeddedObjects
+                {
+                    Author = new UserAPIResourceDTO(publication.Author.Id, listUsersUrl)
+                    {
+                        Username = publication.Author.Username
+                    },
+                    OriginalPost = PostAPIResourceDTO.FromIPost(repost.OriginalPost, urlHelper)
+                }
+            };
+        }
+
+        return new PublicationAPIResourceDTO(publication.Id, listPublicationsUrl)
+        {
+            IsRepost = false,
+            AuthorUsername = publication.Author.Username,
+            PublicationDate = publication.PublicationDate,
+            Content = publication.Content,
+            Embedded = new EmbeddedObjects
+            {
+                Author = new UserAPIResourceDTO(publication.Author.Id, listUsersUrl)
+                {
+                    Username = publication.Author.Username
+                }
+            },
+        };
+    }
+
+    public sealed record EmbeddedObjects
+    {
+        public required UserAPIResourceDTO Author { get; init; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public PostAPIResourceDTO? OriginalPost { get; set; }
+    }
+}
--- BackEnd/Presentation/Web/RestApi/Controllers/Publications/PublicationsListDTO.cs
+++ BackEnd/Presentation/Web/RestApi/Controllers/Publications/PublicationsListDTO.cs
@@ -1,19 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Posterr.Core.Application.UseCases.ListPublicationsWithPagination;
-using Posterr.Core.Boundaries.EntitiesInterfaces;
 using Posterr.Presentation.Web.RestApi.Controllers.HATEOAS.HAL;
 using Posterr.Presentation.Web.RestApi.Controllers.Models;
-using Posterr.Presentation.Web.RestApi.Controllers.Users;
-using System.Text.Json.Serialization;
 
 namespace Posterr.Presentation.Web.RestApi.Controllers.Publications;
 
-public sealed record PublicationsListDTO : APIResource<PublicationsListDTO.PublicationsList>
+public sealed record PublicationsListDTO : APIResource<PublicationsListDTO.EmbeddedObjects>
 {
     public int Count { get; set; }
 
     public PublicationsListDTO(
-        IList<PublicationsListItemDTO> publications,
+        IList<PublicationAPIResourceDTO> publications,
         ListPublicationsUseCaseInputDTO paginationParameters,
         IUrlHelper urlHelper)
     {
@@ -30,83 +27,8 @@ public sealed record PublicationsListDTO : APIResource<PublicationsListDTO.Publi
         }
     }
 
-    public sealed record PublicationsList
+    public sealed record EmbeddedObjects
     {
-        public required List<PublicationsListItemDTO> Publications { get; init; }
-    }
-
-    public sealed record PublicationsListItemDTO : APIResource<PublicationsListItemDTO.EmbeddedObjects>
-    {
-        public required bool IsRepost { get; init; }
-        public long Id { get; }
-        public required string AuthorUsername { get; init; }
-        public required DateTime PublicationDate { get; init; }
-        public required string Content { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string? OriginalPostAuthorUsername { get; set; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DateTime? OriginalPostPublicationDate { get; init; }
-
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string? OriginalPostContent { get; init; }
-
-        public PublicationsListItemDTO(long id, string publicationsListEndpointUrl)
-        {
-            Id = id;
-            Links.Add("self", [new($"{publicationsListEndpointUrl}/{Id}")]);
-        }
-
-        public static PublicationsListItemDTO FromIPublication(IPublication publication, IUrlHelper urlHelper)
-        {
-            string listUsersUrl = urlHelper.ActionLink(nameof(UsersController.ListUsers), nameof(UsersController).Replace("Controller", ""))!;
-            string listPublicationsUrl = urlHelper.ActionLink(nameof(UsersController.ListUsers), nameof(UsersController).Replace("Controller", ""))!;
-
-            if (publication is IRepost repost)
-            {
-                return new PublicationsListItemDTO(publication.Id, listPublicationsUrl)
-                {
-                    IsRepost = true,
-                    AuthorUsername = publication.Author.Username,
-                    PublicationDate = publication.PublicationDate,
-                    Content = publication.Content,
-                    OriginalPostAuthorUsername = repost.OriginalPost.Author.Username,
-                    OriginalPostPublicationDate = repost.OriginalPost.PublicationDate,
-                    OriginalPostContent = repost.OriginalPost.Content,
-                    Embedded = new EmbeddedObjects
-                    {
-                        Author = new UserAPIResourceDTO(publication.Author.Id, listUsersUrl)
-                        {
-                            Username = publication.Author.Username
-                        },
-                        OriginalPost = PostAPIResourceDTO.FromIPost(repost.OriginalPost, urlHelper)
-                    }
-                };
-            }
-
-            return new PublicationsListItemDTO(publication.Id, listPublicationsUrl)
-            {
-                IsRepost = false,
-                AuthorUsername = publication.Author.Username,
-                PublicationDate = publication.PublicationDate,
-                Content = publication.Content,
-                Embedded = new EmbeddedObjects
-                {
-                    Author = new UserAPIResourceDTO(publication.Author.Id, listUsersUrl)
-                    {
-                        Username = publication.Author.Username
-                    }
-                },
-            };
-        }
-
-        public sealed record EmbeddedObjects
-        {
-            public required UserAPIResourceDTO Author { get; init; }
-
-            [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-            public PostAPIResourceDTO? OriginalPost { get; set; }
-        }
+        public required List<PublicationAPIResourceDTO> Publications { get; init; }
     }
 }
