5883cf28fc4ddb9cc567f45df0ea078f9eedb780
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-12 14:23:22 -0300

    fixes tests

--- BackEnd/Core/Application/UseCasesTests/ListPublicationsUseCaseTests.cs
+++ BackEnd/Core/Application/UseCasesTests/ListPublicationsUseCaseTests.cs
@@ -3,17 +3,16 @@ using Posterr.Core.Application.UseCases.ListPublicationsWithPagination;
 using Posterr.Core.Boundaries.ConfigurationInterface;
 using Posterr.Core.Boundaries.EntitiesInterfaces;
 using Posterr.Core.Boundaries.Persistence;
-using Posterr.Core.Shared.Exceptions;
 
 namespace Posterr.Core.Application.UseCasesTests;
 
-public class ListPublicationsWithPaginationUseCaseTests
+public class ListPublicationsUseCaseTests
 {
     private readonly ListPublicationsUseCase useCase;
     private readonly IPublicationsRepository publicationsRepository = Fake.PublicationsRepository();
     private readonly IDomainConfig domainConfig = Fake.DomainConfig();
 
-    public ListPublicationsWithPaginationUseCaseTests()
+    public ListPublicationsUseCaseTests()
     {
         useCase = new(publicationsRepository);
     }
@@ -22,16 +21,16 @@ public class ListPublicationsWithPaginationUseCaseTests
     public void GivenAPageHasPostsAndReposts_ThenRepostsShouldBeFlagged()
     {
         var yesterday = DateTime.UtcNow.AddDays(-1);
-        IUser posterUser = Fake.User(Fake.OriginalPostAuthorUsername);
-        IPost post = Fake.Post(1, posterUser, yesterday, Fake.Content);
+        IUser postAuthor = Fake.User(Fake.OriginalPostAuthorUsername);
+        IPost post = Fake.Post(1, postAuthor, yesterday, Fake.Content);
         
         var now = DateTime.UtcNow;
-        IUser reposterUser = Fake.User(Fake.RepostAuthorUsername);
-        IRepost repost = Fake.Repost(2, reposterUser, now, "repost content", post);
+        IUser repostAuthor = Fake.User(Fake.RepostAuthorUsername);
+        IRepost repost = Fake.Repost(2, repostAuthor, now, "repost content", post);
         
-        A.CallTo(() => publicationsRepository.Paginate(0, 15)).Returns([repost, post]);
+        A.CallTo(() => publicationsRepository.GetNMostRecentPublications(15)).Returns([repost, post]);
 
-        IList<IPublication> publications = useCase.Run(new ListPublicationsUseCaseInputDTO(1, domainConfig));
+        IList<IPublication> publications = useCase.Run(new ListPublicationsUseCaseInputDTO(true, 0, domainConfig));
 
         Assert.Equal(2, publications.Count);
 
@@ -48,15 +47,5 @@ public class ListPublicationsWithPaginationUseCaseTests
         Assert.Equal(Fake.OriginalPostAuthorUsername, publications[1].Author.Username);
         Assert.Equal(yesterday, publications[1].PublicationDate);
         Assert.Equal(Fake.Content, publications[1].Content);
-    }
-
-    [Theory]
-    [InlineData(0)]
-    [InlineData(-1)]
-    public void GivenAnInvalidPageNumber_ThenThrowException(int pageNumber)
-    {
-        Assert.Throws<InvalidPageNumberException>(
-            () => useCase.Run(new ListPublicationsUseCaseInputDTO(pageNumber, domainConfig))
-        );
     }
 }
