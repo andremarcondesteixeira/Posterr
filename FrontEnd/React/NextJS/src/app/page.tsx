603f783e17b476c0ecdd2f9a35b212bbf76e8bda
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-18 16:13:12 -0300

    extracts component PublicationsList

--- FrontEnd/React/NextJS/src/app/page.tsx
+++ FrontEnd/React/NextJS/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { NewPublicationForm } from "@/components/NewPublicationForm";
-import { Publication } from "@/components/Publication";
+import { PublicationsList } from "@/components/PublicationsList";
 import type { PublicationEntity } from "@Core/Domain/Entities/types";
 import { ApiEndpoint, PublicationAPIResource } from "@Core/Services/ApiEndpointsService";
 import { REQUEST_ABORTED } from "@Core/Services/HttpRequestService";
@@ -68,22 +68,7 @@ export default function Home() {
         originalPost={originalPostForRepost}
         cancelRepostAction={() => setOriginalPostForRepost(null)}
       />
-      <ul className={styles.publicationsList}>
-        {publications && publications.map(publication => (
-          <li key={publication.id}>
-            <Publication publication={publication}>
-              {!publication.isRepost && (
-                <button className="transparent" type="button" onClick={() => startRepost(publication)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path d="M5 4a2 2 0 0 0-2 2v6H0l4 4 4-4H5V6h7l2-2H5zm10 4h-3l4-4 4 4h-3v6a2 2 0 0 1-2 2H6l2-2h7V8z" />
-                  </svg>
-                  Repost
-                </button>
-              )}
-            </Publication>
-          </li>
-        ))}
-      </ul>
+      <PublicationsList publications={publications} startRepostAction={startRepost} />
       <p ref={feedEndElementRef}>That's all for today!</p>
     </main>
   );
