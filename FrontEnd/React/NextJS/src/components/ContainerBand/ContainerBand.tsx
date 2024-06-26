435a2e4fa9ee1367e90d8e41c8c1310f59116bd5
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-25 21:37:01 -0300

    ContainerBand can get class names through props

--- FrontEnd/React/NextJS/src/components/ContainerBand/ContainerBand.tsx
+++ FrontEnd/React/NextJS/src/components/ContainerBand/ContainerBand.tsx
@@ -3,10 +3,11 @@ import styles from "./ContainerBand.module.css";
 
 type Props = {
   children: ReactNode;
+  className?: string;
 }
 
-export function ContainerBand({ children }: Props) {
+export function ContainerBand({ children, className }: Props) {
   return (
-    <div className={styles.containerBand}>{children}</div>
+    <div className={`${styles.containerBand} ${className}`}>{children}</div>
   );
 }
