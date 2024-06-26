4117bde594d5434eb118bdf7228b1e0db6f69471
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2024-06-25 21:33:38 -0300

    SearchForm component

---
+++ FrontEnd/React/NextJS/src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { SearchIcon } from "../Icons";
+import styles from "./SearchForm.module.css";
+
+type Props = {
+  initialValue?: string | null;
+}
+
+export function SearchForm({ initialValue }: Props) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setSearchTerm(initialValue || "");
+  }, []);
+
+  return (
+    <form action="/search" method="GET" className={styles.searchForm}>
+      <input
+        type="text"
+        placeholder="Search"
+        name="searchTerm"
+        value={searchTerm}
+        onChange={event => setSearchTerm(event.target.value)}
+        required
+      />
+      <button className="primary" type="submit">
+        <SearchIcon color="white" />
+        Search
+      </button>
+    </form>
+  );
+}
